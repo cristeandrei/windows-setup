821c84fba99b2226ecd1d3d67a329090ed29a032
acriste <cristeandreiflorin@gmail.com>
2025-10-07 15:25:13 +0300

    added script for installing apps for fresh windows install

---
+++ winget-install.ps1
@@ -0,0 +1,17 @@
+winget install --id Microsoft.PowerToys --source winget
+
+winget install --id=Microsoft.VisualStudioCode.Insiders  -e
+
+winget install --id=Google.Chrome.Beta  -e
+
+winget install --id=Git.Git  -e
+
+winget install --id JetBrains.Toolbox -e
+
+winget install --id=qBittorrent.qBittorrent  -e
+
+winget install -e --id Docker.DockerDesktop
+
+winget install -e --id Nvidia.GeForceExperience
+
+winget install -e --id Valve.Steam
